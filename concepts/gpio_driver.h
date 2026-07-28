27b34cedb0369f97098c1ec0c8615fd6324503c8
Srihari2651 <harisri2651@gmail.com>
2026-07-28 10:02:45 +0530

    Add GPIO driver header file for STM32F103
    - Added RCC and GPIOC register definitions
    - Defined onboard LED pin (PC13)
    - Added GPIO state enum
    - Declared GPIO driver function prototypes
    - Prepared header for bare-metal STM32 programming

--- concepts/gpio_driver.h
+++ concepts/gpio_driver.h
@@ -3,34 +3,26 @@
 
 #include <stdint.h>
 
-/*--------------------------------------------------
-   RCC Registers
---------------------------------------------------*/
+/* RCC Registers */
 #define RCC_BASE        0x40021000UL
 #define RCC_APB2ENR     (*(volatile uint32_t *)(RCC_BASE + 0x18))
 
-/*--------------------------------------------------
-   GPIOC Registers
---------------------------------------------------*/
+/* GPIOC Registers */
 #define GPIOC_BASE      0x40011000UL
 #define GPIOC_CRH       (*(volatile uint32_t *)(GPIOC_BASE + 0x04))
 #define GPIOC_ODR       (*(volatile uint32_t *)(GPIOC_BASE + 0x0C))
 
-/*--------------------------------------------------
-   Onboard LED
---------------------------------------------------*/
+/* LED Pin */
 #define LED_PIN         13U
 
-/* GPIO Output States */
 typedef enum
 {
     GPIO_LOW = 0,
     GPIO_HIGH = 1
 } gpio_state_t;
 
-/* Function Prototypes */
 void gpio_led_init(void);
 void gpio_led_write(gpio_state_t state);
 void gpio_led_toggle(void);
 
-#endif /* GPIO_DRIVER_H */
+#endif
