9c5cea93495ee5b428b58b08caddeae335487e60
Srihari2651 <harisri2651@gmail.com>
2026-07-30 10:45:40 +0530

    Update and rename gpio_driver.h to fibonacci.c
    Today I have created a Basic C programing code to understand the structure of writing a code before structuring a strong Embedded Program

---
+++ concepts/fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c;
+
+    printf("Fibonacci Series:\n");
+
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
